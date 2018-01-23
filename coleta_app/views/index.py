68f582866ebd30743bcc8df29eb4f769da4da263
Wagner Esser <waesser@gmail.com>
2018-01-23 12:51:16 -0200

    Ajustes no template da tela principal; e aplicação de filtro nas consultas da view index.

--- coleta_app/views/index.py
+++ coleta_app/views/index.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from coleta_app.models.pessoa import PessoaModel
 from coleta_app.models.projeto import ProjetoModel
 from coleta_app.models.coleta import ColetaModel
+from django.db.models import Q
 
 
 def index(request):
@@ -11,7 +12,11 @@ def index(request):
 
     context_dict = {}
     # usuario = PessoaModel.objects.get(pk=2)
-    projetos = ProjetoModel.objects.all()
+    id_user = request.user.pk
+    print(id_user)
+    projetos = ProjetoModel.objects.filter(
+        Q(pessoa_projeto__pessoa=id_user) |
+        Q(orientador=id_user)).order_by('nome')
     coletas = ColetaModel.objects.filter(projeto__in=projetos)
 
     context_dict['projetos'] = projetos
