44d7fc0ca8aa1feec3f4affe68c512e910b6b94e
Wagner Esser <waesser@gmail.com>
2018-02-27 14:10:06 -0300

    Recepção dos dados concluído (recebendo e cadastrando no BD; mostrando no template).

--- coleta_app/views/dados.py
+++ coleta_app/views/dados.py
@@ -13,68 +13,99 @@ from coleta_app.views.index import index
 
 
 def novo_dado(request):
-    msg = None
-    tipo_msg = None
 
     if request.GET:
         if 'token' in request.GET:
-            print("Requisição chegou...")
-
-            print("TOKEN: ")
-            print(request.GET['token'])
-
-            coleta = ColetaModel.objects.get(token=request.GET['token'])
-            print("ID da coleta: ")
-            print(coleta.id)
-
-            if 'local' in request.GET:
-                print("Local: ")
-                print(request.GET['local'])
-
-            if 'data' in request.GET:
-                print("Data: ")
-                print(request.GET['data'])
-
-            if 'hora' in request.GET:
-                print("Hora: ")
-                print(request.GET['hora'])
-
-            if 'sensor' in request.GET:
-                print("Sensor: ")
-                print(request.GET['sensor'])
-
-            if 'tipo_sensor' in request.GET:
-                print("Tipo do sensor: ")
-                print(request.GET['tipo_sensor'])
-
             if 'dado' in request.GET:
+                print("Requisição chegou...")
+
+                print("TOKEN: ")
+                print(request.GET['token'])
+
                 print("Dado: ")
                 print(request.GET['dado'])
 
-            if 'unidade_medida' in request.GET:
-                print("Unidade de medida: ")
-                print(request.GET['unidade_medida'])
+                try:
+                    coleta = ColetaModel.objects.get(token=request.GET['token'])
+                except:
+                    msg = "Não foi encontrado dados para este TOKEN!"
+                    tipo_msg = "red"
+                    print(msg)
+                    return index(request, msg, tipo_msg)
 
-            try:
-                DadosModel.objects.create(nome_projeto=request.GET['nome_projeto'],
-                                          orientador_projeto=request.GET['orientador_projeto'],
-                                          coleta_id=request.GET['coleta_id'],
-                                          contexto=request.GET['contexto'],
-                                          local=request.GET['local'],
-                                          data=request.GET['data'],
-                                          hora=request.GET['hora'],
-                                          sensor=request.GET['sensor'],
-                                          tipo_sensor=request.GET['tipo_sensor'],
-                                          dado=request.GET['dado'],
-                                          unidade_medida=request.GET['unidade_medida'],
-                                          id_controlador=request.GET['id_controlador'],
-                                          token=request.GET['token'],
-                                          )
-                msg = "Nova requisição chegou --> Dados gravados com sucesso"
-                tipo_msg = 'green'
-            except:
-                msg = "Nova requisição cehgou --> ERRO! Os dados não foram gravados"
-                tipo_msg = 'red'
+                print("ID da coleta: ")
+                print(coleta.id)
+
+                nome_projeto = coleta.projeto.nome
+                orientador_projeto = coleta.projeto.orientador.get_full_name()
+                dado = request.GET['dado']
+
+                if 'local' in request.GET:
+                    print("Local: ")
+                    print(request.GET['local'])
+                    local = request.GET['local']
+                else:
+                    local = ""
+
+                if 'data' in request.GET:
+                    print("Data: ")
+                    print(request.GET['data'])
+                    data = request.GET['data']
+                else:
+                    data = ""
+
+                if 'hora' in request.GET:
+                    print("Hora: ")
+                    print(request.GET['hora'])
+                    hora = request.GET['hora']
+                else:
+                    hora = ""
+
+                if 'sensor' in request.GET:
+                    print("Sensor: ")
+                    print(request.GET['sensor'])
+                    sensor = request.GET['sensor']
+                else:
+                    sensor = ""
+
+                if 'tipo_sensor' in request.GET:
+                    print("Tipo do sensor: ")
+                    print(request.GET['tipo_sensor'])
+                    tipo_sensor = request.GET['tipo_sensor']
+                else:
+                    tipo_sensor = ""
+
+                if 'unidade_medida' in request.GET:
+                    print("Unidade de medida: ")
+                    print(request.GET['unidade_medida'])
+                    unidade_medida = request.GET['unidade_medida']
+                else:
+                    unidade_medida = ""
+
+                try:
+                    DadosModel.objects.create(nome_projeto=nome_projeto,
+                                              orientador_projeto=orientador_projeto,
+                                              coleta_id=coleta.id,
+                                              contexto=coleta.contexto,
+                                              local=local,
+                                              data=data,
+                                              hora=hora,
+                                              sensor=sensor,
+                                              tipo_sensor=tipo_sensor,
+                                              dado=dado,
+                                              unidade_medida=unidade_medida,
+                                              id_controlador=coleta.id_controlador,
+                                              )
+                    msg = "Nova requisição chegou --> Dados gravados com sucesso"
+                    tipo_msg = 'green'
+                except:
+                    msg = "Nova requisição chegou --> ERRO! Os dados não foram gravados"
+                    tipo_msg = 'red'
+                print(msg)
+            else:
+                msg = "Não foi encontrado o dado coletado."
+                tipo_msg = "red"
+                print(msg)
         else:
             msg = "É necessário um token de autenticação."
             tipo_msg = "red"
